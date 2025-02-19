6c573c71ab0afdab7ee6671acfdf7504731fd224
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-02-19 12:51:57 +0530

    array of names

---
+++ src/assets/names.ts
@@ -0,0 +1,13 @@
+const codenames = [
+    "The Nightingale", "The Kraken", "The Phantom", "The Viper",
+    "The Shadow", "The Falcon", "The Cobra", "The Specter",
+    "The Mirage", "The Thunderbolt", "The Scorpion", "The Enigma",
+    "The Wraith", "The Eclipse", "The Sentinel", "The Ghost",
+    "The Tempest", "The Blackhawk", "The Oracle", "The Lynx",
+    "The Predator", "The Reaper", "The Chimera", "The Nomad",
+    "The Phoenix", "The Stormrider", "The Ironclad", "The Titan",
+    "The Maverick", "The Serpent"
+  ];
+  
+  export default codenames;
+  
